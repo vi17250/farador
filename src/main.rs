45cb76187d89565709ae683871c4139536ebdcbc
vi17250 <v.aubinaud@protonmail.com>
2025-06-23 14:25:35 +0200

    feat: 🎸 "Arme" is a option AND only "Archimage" can fireball

--- src/main.rs
+++ src/main.rs
@@ -18,11 +18,11 @@ struct Personnage {
     niveau: usize,
     classe: Classe,
     stock: usize,
-    arme: Arme,
+    arme: Option<Arme>,
 }
 
 impl Personnage {
-    fn new(nom: String, niveau: usize, classe: Classe, stock: usize, arme: Arme) -> Self {
+    fn new(nom: String, niveau: usize, classe: Classe, stock: usize, arme: Option<Arme>) -> Self {
         Personnage {
             nom,
             niveau,
@@ -31,10 +31,23 @@ impl Personnage {
             arme,
         }
     }
+
+    fn fireball(&self) -> Result<(), &'static str> {
+        if self.classe == Classe::Archimage {
+            return Ok(());
+        }
+        Err("Seul le mage peut lancer une boule de feu")
+    }
 }
 
 fn main() {
-    let personnage = Personnage::new("Gardakan".to_string(), 66, Classe::Paladin, 0, Arme::Epee);
+    let personnage = Personnage::new(
+        "Gardakan".to_string(),
+        66,
+        Classe::Paladin,
+        0,
+        Some(Arme::Epee),
+    );
     dbg!(personnage);
 }
 
@@ -43,9 +56,14 @@ mod test {
     use crate::*;
 
     #[test]
-    fn it_creates_a_character() {
-        let perso: Personnage =
-            Personnage::new("Gardakan".to_string(), 66, Classe::Paladin, 0, Arme::Epee);
+    fn it_creates_a_character_with_a_weapon() {
+        let perso: Personnage = Personnage::new(
+            "Gardakan".to_string(),
+            66,
+            Classe::Paladin,
+            0,
+            Some(Arme::Epee),
+        );
         assert_eq!(
             perso,
             Personnage {
@@ -53,8 +71,40 @@ mod test {
                 niveau: 66,
                 classe: Classe::Paladin,
                 stock: 0,
-                arme: Arme::Epee
+                arme: Some(Arme::Epee)
             }
         );
+    }
+
+    #[test]
+    fn it_creates_a_character_without_a_weapon() {
+        let perso: Personnage =
+            Personnage::new("Gardakan".to_string(), 66, Classe::Paladin, 0, None);
+        assert_eq!(
+            perso,
+            Personnage {
+                nom: String::from("Gardakan"),
+                niveau: 66,
+                classe: Classe::Paladin,
+                stock: 0,
+                arme: None
+            }
+        );
+    }
+
+    #[test]
+    fn it_launches_a_fireball() {
+        let perso: Personnage =
+            Personnage::new("Gardakan".to_string(), 66, Classe::Archimage, 0, None);
+        let shoot = perso.fireball();
+        assert_eq!(shoot, Ok(()));
+    }
+
+    #[test]
+    fn it_cannot_launches_a_fireball() {
+        let perso: Personnage =
+            Personnage::new("Gardakan".to_string(), 66, Classe::Paladin, 0, None);
+        let shoot = perso.fireball();
+        assert_eq!(shoot, Err("Seul le mage peut lancer une boule de feu"));
     }
 }
