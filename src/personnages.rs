6c4c4c8d8b9103598e4cbb5d9bda5aadc5a1b8f6
vi17250 <v.aubinaud@protonmail.com>
2025-06-26 12:16:32 +0200

    refactor: 💡 crate 'Personnage'

---
+++ src/personnages.rs
@@ -0,0 +1,131 @@
+#[derive(Debug, PartialEq, Eq)]
+pub enum Classe {
+    Paladin,
+    Archimage,
+    Voleur,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Arme {
+    Epee,
+    Baton,
+    Dague,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Personnage {
+    nom: String,
+    niveau: usize,
+    classe: Classe,
+    stock: usize,
+    arme: Option<Arme>,
+}
+
+impl Personnage {
+    pub fn new(
+        nom: String,
+        niveau: usize,
+        classe: Classe,
+        stock: usize,
+        arme: Option<Arme>,
+    ) -> Self {
+        Personnage {
+            nom,
+            niveau,
+            classe,
+            stock,
+            arme,
+        }
+    }
+
+    pub fn fireball(&self) -> Result<(), &'static str> {
+        if self.classe == Classe::Archimage {
+            return Ok(());
+        }
+        Err("Seul le mage peut lancer une boule de feu")
+    }
+
+    pub fn take_weapon(&mut self) -> Option<Arme> {
+        self.arme.take()
+    }
+}
+
+mod test {
+    use crate::*;
+
+    #[test]
+    fn it_creates_a_character_with_a_weapon() {
+        let perso: Personnage = Personnage::new(
+            "Gardakan".to_string(),
+            66,
+            Classe::Paladin,
+            0,
+            Some(Arme::Epee),
+        );
+        assert_eq!(
+            perso,
+            Personnage {
+                nom: String::from("Gardakan"),
+                niveau: 66,
+                classe: Classe::Paladin,
+                stock: 0,
+                arme: Some(Arme::Epee)
+            }
+        );
+    }
+
+    #[test]
+    fn it_creates_a_character_without_a_weapon() {
+        let perso: Personnage =
+            Personnage::new("Gardakan".to_string(), 66, Classe::Paladin, 0, None);
+        assert_eq!(
+            perso,
+            Personnage {
+                nom: String::from("Gardakan"),
+                niveau: 66,
+                classe: Classe::Paladin,
+                stock: 0,
+                arme: None
+            }
+        );
+    }
+
+    #[test]
+    fn it_launches_a_fireball() {
+        let perso: Personnage =
+            Personnage::new("Gardakan".to_string(), 66, Classe::Archimage, 0, None);
+        let shoot = perso.fireball();
+        assert_eq!(shoot, Ok(()));
+    }
+
+    #[test]
+    fn it_cannot_launches_a_fireball() {
+        let perso: Personnage =
+            Personnage::new("Gardakan".to_string(), 66, Classe::Paladin, 0, None);
+        let shoot = perso.fireball();
+        assert_eq!(shoot, Err("Seul le mage peut lancer une boule de feu"));
+    }
+
+    #[test]
+    fn it_returns_weapon() {
+        let mut perso: Personnage = Personnage::new(
+            "Gardakan".to_string(),
+            66,
+            Classe::Paladin,
+            0,
+            Some(Arme::Epee),
+        );
+        let taken = perso.take_weapon();
+        assert_eq!(taken, Some(Arme::Epee));
+        assert_eq!(perso.arme, None);
+    }
+
+    #[test]
+    fn it_returns_none() {
+        let mut perso: Personnage =
+            Personnage::new("Gardakan".to_string(), 66, Classe::Paladin, 0, None);
+        let taken = perso.take_weapon();
+        assert_eq!(taken, None);
+        assert_eq!(perso.arme, None);
+    }
+}
